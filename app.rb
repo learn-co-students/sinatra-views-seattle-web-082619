f2196692e9c55229756be0416db87eeab2f87098
renburnett <renardburnett@gmail.com>
2019-09-16 17:14:38 -0700

    implemented tings

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index #this tells sinatra to render a file called `index.irb` inside of a directory called `views`
+	end
+
+	get '/info' do
+		erb :info
 	end
 end
